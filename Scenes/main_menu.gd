1b681109e4aa0baf9a294e81fb87bd465c7c0bbb
jelani_j <jelnjoseph4@gmail.com>
2026-01-24 20:55:36 -0500

    added main menu with functioning scene transfers

---
+++ Scenes/main_menu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+@onready var menu_label = $PanelContainer/Label
+@onready var menu_buttons = [$VBoxContainer/Options_Button, $VBoxContainer/New_Game_Button, $VBoxContainer/Other_Button]
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_new_game_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Quiz-Components/Front-Screen.tscn")
+
+
+func _on_options_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_other_button_pressed() -> void:
+	pass # Replace with function body.
