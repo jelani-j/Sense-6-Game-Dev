1b8c966f8ade43f238160ec7ee4dd6c3871f1771
jelani_j <jelnjoseph4@gmail.com>
2026-02-28 22:09:52 -0500

    messing around with resources trying to create monster library

---
+++ Scenes/Combat/battle_unit.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+signal unit_clicked(unit)
+
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var area: Area2D = $Area2D
+
+var unit_name: String
+var max_hp: int
+var current_hp: int
+var attack: int
+var defense: int
+var is_enemy: bool = false
+
+func _ready():
+	area.input_event.connect(_on_input_event)
+
+func setup(data: Dictionary):
+	unit_name = data.name
+	max_hp = data.max_hp
+	current_hp = max_hp
+	attack = data.attack
+	defense = data.defense
+	sprite.texture = data.texture
+
+func _on_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.pressed:
+		emit_signal("unit_clicked", self)
+
+func take_damage(amount: int):
+	current_hp -= amount
+	print(unit_name, "HP:", current_hp)
+
+	if current_hp <= 0:
+		die()
+
+func die():
+	queue_free()
