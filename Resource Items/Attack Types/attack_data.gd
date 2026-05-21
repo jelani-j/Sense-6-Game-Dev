351672ad897d02e36897e82cc4c1116697f755a5
jelani_j <jelnjoseph4@gmail.com>
2026-05-20 20:28:59 -0400

    added more details to attack system, by using export enum discovered way to allow drop down options for tres files

--- Resource Items/Attack Types/attack_data.gd
+++ Resource Items/Attack Types/attack_data.gd
@@ -4,4 +4,9 @@ class_name AttackData
 @export var name: String
 @export var damage: int
 @export var cost: int
+@export_enum("Blunt", "Slicing", "Piercing", "Ballistic", "Emission", "Soul") var attack_type
+@export_enum("Poison", "Fire", "Stun", "Electrified", "Bleeding", "Soul Shatterd") var status
+@export var status_chance: float
+@export var aoe: bool
+@export var speed: float 
 @export var defense_penetration: int
